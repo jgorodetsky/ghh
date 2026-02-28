a8c70c44f7896c89e9ca3607547c4f31f5e210e1
jgorodetsky <jakegorodetsky@gmail.com>
2026-02-27 23:05:24 -0500

    fix: add release SHA256 to Homebrew formula

--- homebrew/ghh.rb
+++ homebrew/ghh.rb
@@ -6,7 +6,7 @@ class Ghh < Formula
   desc "Your entire git + GitHub workflow in one searchable command"
   homepage "https://github.com/jgorodetsky/ghh"
   url "https://github.com/jgorodetsky/ghh/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "956adccd280c06bf851faf0be76b49189c88bd42617623b7ad7d3fa896b0e5f3"
   license "MIT"
 
   depends_on "fzf"
